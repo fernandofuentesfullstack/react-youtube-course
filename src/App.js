c0382b48666e3f9bf88a74b34384fc1f4d70f230
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2021-01-25 21:36:50 +0100

    feat: #video2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { useState } from "react";
 import Form from "./Components/Form";
+import "./App.css";
 
 function App() {
-  const [firstName, setFirstName] = useState('FullStack');
-  const [age, setAge] = useState(0);
-
   return (
     <div className="App">
-      {firstName} <button onClick={() => setFirstName('Fernando')}>Change Name</button>
-      <br />
-      {age} <button onClick={() => setAge(age + 1)}>Increase Age</button>
-
-      <Form firstName={"Fernando"} lastName={"Fuentes"} email={"f@gmail.com"} />
       <Form />
     </div>
   );
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Form.css";
 
-export default function Form({ firstName, lastName, email }) {
-    return (
-        <div>
-            <h1>First Name: {firstName} </h1>
-            <h1>Last Name: {lastName} </h1>
-            <h1>Email: {email} </h1>
-            <hr />
-        </div>
-    )
+export default function Form() {
+  const username = "FernandoFuentesFullStack";
+  const password = "Subscribe";
+
+  const [usernameState, setUsernameState] = useState("");
+  const [passwordState, setPasswordState] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  function login () {
+      if (usernameState === username && passwordState === password) {
+          setLoggedIn(true);
+      }
+  }
+
+  return (
+    <div className="Form">
+      <h1>Login</h1>
+      <input
+        type="text"
+        onChange={(event) => {
+          setUsernameState(event.target.value);
+          console.log(event.target.value);
+          console.log(usernameState);
+        }}
+      />
+      <input
+        type="password"
+        onChange={(event) => {
+          setPasswordState(event.target.value);
+          console.log(event.target.value);
+          console.log(passwordState);
+        }}
+      />
+      <button onClick={login}>Submit</button>
+
+      {loggedIn && <h1>Logged In</h1>}
+    </div>
+  );
 }
