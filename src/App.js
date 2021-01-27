d00cf8129c63c84a3019fe89962251f2fd8f40aa
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2021-01-27 09:41:37 +0100

    feat: clean project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [number, setNumber] = useState(0);
-
-  useEffect(() => {
-    console.log('page rendered')
-  }, [number])
-
+  
   return (
-    <div className="App" style={{ fontSize: 30 }}>
-      {number}
-      <button
-        onClick={() => {
-          setNumber(number + 1);
-        }}
-      >
-        {" "}
-        Increase Number
-      </button>
-
+    <div className="App">
+      <h1>Fernando Fuentes FullStack</h1>
+      <h2>¡¡ Suscríbete !!</h2>
     </div>
   );
 }
