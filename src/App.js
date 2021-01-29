75dcd5b86da5bdeed25186371e960421d1f15d25
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2021-01-29 18:50:21 +0100

    Added #video8

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Fernando Fuentes FullStack</h1>
-      <h2>Curso React #7</h2>
+      <h2>Curso React #8</h2>
       <h2>¡¡ Suscríbete !!</h2>
     </div>
   );
